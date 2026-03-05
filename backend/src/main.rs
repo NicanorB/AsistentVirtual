95b50564a0c9227abc63228a7b6363b4a3b44c3b
Nicu Borta <nicu.borta111@gmail.com>
2026-03-05 08:40:17 +0200

    backend: switch to using axum
    Replace rocket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,36 +1,13 @@
-use rocket::{
-    Route, get, launch, routes,
-    serde::{Serialize, json::Json},
-};
-use rocket_db_pools::{
-    Connection, Database,
-    diesel::{self, QueryResult},
-};
+use axum::{Json, Router, routing::get};
+use serde::Serialize;
+use std::net::SocketAddr;
 
 #[derive(Serialize)]
-#[serde(crate = "rocket::serde")]
 struct Settings {
     name: String,
     status: String,
 }
 
-/*
-#[derive(Database)]
-#[database("asistentvirtual-db")]
-pub struct Db(diesel::PgPool);
-
-#[get("/get_users")]
-async fn get_users(mut db: Connection<Db>) -> QueryResult<String> {
-    Ok("test".to_string())
-}
-*/
-
-#[get("/get_users")]
-async fn get_users() -> String {
-    "test".to_string()
-}
-
-#[get("/get_settings")]
 async fn get_settings() -> Json<Settings> {
     Json(Settings {
         name: "AsistentVirtual".to_string(),
@@ -38,14 +15,12 @@ async fn get_settings() -> Json<Settings> {
     })
 }
 
-pub fn routes() -> Vec<Route> {
-    routes![get_users, get_settings]
-}
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/get_settings", get(get_settings));
 
-#[launch]
-fn rocket() -> _ {
-    /*
-    rocket::build().attach(Db::init()).mount("/api", routes())
-    */
-    rocket::build().mount("/api", routes())
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://backend:8000",
+        target: "http://backend:3000",
         changeOrigin: true,
       },
     },
